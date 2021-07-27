88a1503f207903f93eb0687df122e89239c339c0
Inácio Fabrício Nunes de Sousa <inacio.fabricio.nunes@gmail.com>
2021-07-27 12:09:36 -0300

    Fim do curso

---
+++ Alura.Filmes.App/Program.cs
@@ -0,0 +1,51 @@
+﻿using Alura.Filmes.App.Dados;
+using Alura.Filmes.App.Extensions;
+using Microsoft.EntityFrameworkCore;
+using System.Data.SqlClient;
+
+namespace Alura.Filmes.App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var contexto = new AluraFilmesContexto())
+            {
+                contexto.LogSQLToConsole();
+
+                var sql = "INSERT INTO language (name) VALUES ('Teste 1'), ('Teste 2'), ('Teste 3')";
+                var registros = contexto.Database.ExecuteSqlCommand(sql);
+                System.Console.WriteLine($"O total de registros afetados é {registros}.");
+
+                var deleteSql = "DELETE FROM language WHERE name LIKE 'Teste%'";
+                registros = contexto.Database.ExecuteSqlCommand(deleteSql);
+                System.Console.WriteLine($"O total de registros afetados é {registros}.");
+
+
+            }
+        }
+
+        static void StoredProcedure(DbContext contexto)
+        {
+            var categ = "Action"; //36
+
+            var paramCateg = new SqlParameter("category_name", categ);
+
+            var paramTotal = new SqlParameter
+            {
+                ParameterName = "@total_actors",
+                Size = 4,
+                Direction = System.Data.ParameterDirection.Output
+            };
+
+            contexto.Database
+                .ExecuteSqlCommand(
+                  "total_actors_from_given_category @category_name, @total_actors OUT",
+                  paramCateg,
+                  paramTotal);
+
+            System.Console.WriteLine($"O total de atores na categoria {categ} é de {paramTotal.Value}.");
+
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Formação C#
+
+Projetos de exemplo seguindo os cursos da Formação em Asp.NET Core REST APIs da Alura.
+
+- Curso 6 - Entity Framework Core: Mapeando um banco pré-existente parte 2
